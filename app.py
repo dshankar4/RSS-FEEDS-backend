58d4b2148bb7ea11c56d7a2645088e8587d2a8e9
dshankar4 <shankar.narayan3@gmail.com>
2020-06-22 12:50:05 +0530

    api for fetching data

--- app.py
+++ app.py
@@ -62,6 +62,72 @@ class adminRegister(Resource):
         else:
             return {'access_token': 'None'}, 401
 
+# Returns category
+class categoryList(Resource):
+    def get(self):
+        return {'category': category}, 200
+
+# Get Feed by passing id
+class getFeedById(Resource):
+    def get(self,feedId):
+        if checkFeedId(feedId):
+            return {'format':'True','feeds': post, 'id': 'True'}, 200
+        else:
+            return {'format':'False'}, 401
+
+
+# Returns feeds,keys of feeds,apply sorts and filters.
+# Feeds returned after all the filters and sorts
+class getValuesById(Resource):
+    def get(self,category,filterType,order,time,page,key=None,search=None):
+        feeds=[]
+        records=returnRecord()
+        recordsNoRep=returnNoRepRecord(records)
+        result = filtersort(category.capitalize(),filterType,order,time,records,recordsNoRep,key,search)
+        if filterType == "likes":
+            if len(result[0][filterType][int(key)])%10 < 5:
+                pageno=round(len(result[0][filterType][int(key)])/10)+1
+            else:
+                pageno=round(len(result[0][filterType][int(key)])/10)
+            if pageno == page:
+                i=page
+                lp=len(result[0][filterType][int(key)])-((round(len(result[0][filterType][int(key)])/10)))*10
+                print("feeds2",lp)
+                for i in range(i*10-10,(i*10-10)+lp):
+                    feeds.append(result[0][filterType][int(key)][i])
+            elif pageno > page: 
+                i=page
+                for i in range(i*10-10,i*10):
+                    feeds.append(result[0][filterType][int(key)][i])
+            else:
+                return {'Format': 'False'} ,400
+        else:
+            if len(result[0][filterType][key.capitalize()])%10 < 5:
+                pageno=round(len(result[0][filterType][key.capitalize()])/10)+1
+            else:
+                pageno=round(len(result[0][filterType][key.capitalize()])/10) 
+            if pageno == page:
+                i=page
+                lp=len(result[0][filterType][key.capitalize()])%10
+                for i in range(i*10-10,i*10-10+lp):
+                    feeds.append(result[0][filterType][key.capitalize()][i])
+            elif pageno > page: 
+                i=page
+                for i in range(i*10-10,i*10):
+                    feeds.append(result[0][filterType][key.capitalize()][i])
+            else:
+                return {'Format': 'False'} ,400
+        return feeds, 200
+
+# Get feed values
+class getValues(Resource):
+    def get(self,category,filterType,order,time,key=None,search=None):
+        records=returnRecord()
+        recordsNoRep=returnNoRepRecord(records)
+        result = filtersort(category.capitalize(),filterType,order,time,records,recordsNoRep,key,search)
+        return result, 200
+
+
 @app.route('/')
 def hello():
     return "Hello World!!!"
